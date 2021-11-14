072b6c11a85a35d65a45d1e92cd76cddf8d2ea84
benl95 <beno_1995@hotmail.com>
2021-11-14 16:49:27 +0100

    chore: config webpack to import csv files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.csv$/,
+            loader: 'csv-loader',
+            options: {
+                dynamicTyping: true,
+                header: true,
+                skipEmptyLines: true,
+            },
+        });
+
+        return config;
+    },
+};
