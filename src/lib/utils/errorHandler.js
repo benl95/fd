89904eb595992b92bb84720a94d6ae569ea65d31
benl95 <beno_1995@hotmail.com>
2021-11-16 15:41:50 +0100

    build: remove react

--- src/lib/utils/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export default function errorHandler(response) {
-    if (!response.ok) throw new Error(response.status);
-    return response;
-}
--- src/pages/api/coins.js
+++
@@ -1,8 +0,0 @@
-import btcData from '../../public/btcData.csv';
-import { toLowerCase } from '../../lib/utils/toLowerCase';
-
-export default async function getCoinData(req, res) {
-    if (!btcData) return res.status(404).json({ notFound: true });
-    const lowerCased = btcData.map(toLowerCase);
-    res.status(200).json(lowerCased);
-}
