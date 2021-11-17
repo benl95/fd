1ba378ff6c2a74ffe32f9ba70e65d17f58cd817b
benl95 <beno_1995@hotmail.com>
2021-11-17 14:59:09 +0100

    refactor: clean up func

--- src/modules/BarChart.js
+++ src/modules/BarChart.js
@@ -1,4 +1,5 @@
 import {
+	csv,
 	select,
 	scaleLinear,
 	max,
@@ -13,12 +14,38 @@ const svg = select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
-export function BarChart(data, { label, yLabel }) {
-	// Dimensions
-	const margin = { top: 30, right: 20, bottom: 70, left: 70 };
-	const innerWidth = width - margin.left - margin.right;
-	const innerHeight = height - margin.top - margin.bottom;
+// Dimensions
+const margin = { top: 30, right: 20, bottom: 70, left: 70 };
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
 
+// Create group
+const g = svg
+	.append('g')
+	.attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const xScale = scaleLinear().range([0, innerWidth]);
+const yScale = scaleBand().range([0, innerHeight]).padding(0.1);
+
+// Title X Scale
+g.append('text')
+	.attr('y', 350)
+	.attr('x', innerWidth / 2 - 50)
+	.attr('fill', 'black')
+	.text('Price per Coin')
+	.style('font-size', '14px')
+	.style('font-family', 'sans-serif');
+
+// Title Chart
+g.append('text')
+	.attr('class', 'title')
+	.attr('dy', -15)
+	.attr('text-anchor', 'start')
+	.text('Coin currency price trend last 5 years')
+	.style('font-size', '14px')
+	.style('font-family', 'sans-serif');
+
+export function BarChart(data) {
 	// Data
 	const xValue = (d) => d.price;
 	const yValue = (d) => d.date;
@@ -27,51 +54,28 @@ export function BarChart(data, { label, yLabel }) {
 	const dollarFormat = (d) => `$${format(',.2f')(d)}`;
 
 	// Bar rectangles
-	const xScale = scaleLinear()
-		.domain([0, max(data, xValue)])
-		.range([0, innerWidth]);
-	const yScale = scaleBand()
-		.domain(data.map(yValue))
-		.range([0, innerHeight])
-		.padding(0.1);
+	xScale.domain([0, max(data, xValue)]);
+	yScale.domain(data.map(yValue));
 
-	// SVG Element in variable g
-	const g = svg
-		.append('g')
-		.attr('transform', `translate(${margin.left}, ${margin.top})`);
+	// Y Scale
+	g.append('g')
+		.call(axisLeft(yScale))
+		.selectAll('.domain, .tick line')
+		.remove();
 
-	// X & Y Axis
-	const yAxisGroup = g.append('g').call(axisLeft(yScale));
-	yAxisGroup.selectAll('.domain, .tick line').remove();
-
-	const xAxisGroup = g
-		.append('g')
+	// X Scale
+	g.append('g')
 		.call(
 			axisBottom(xScale).tickFormat(dollarFormat).tickSize(-innerHeight)
 		)
-		.attr('transform', `translate(0, ${innerHeight})`);
-	xAxisGroup.select('.domain').remove();
-	xAxisGroup
-		.append('text')
-		.attr('y', 40)
-		.attr('x', innerWidth / 2)
-		.attr('fill', 'black')
-		.text(yLabel)
-		.style('font-size', '14px');
-
-	g.append('text')
-		.attr('class', 'title')
-		.attr('dy', -15)
-		.attr('text-anchor', 'start')
-		.text(label)
-		.style('font-size', '14px')
-		.style('font-family', 'sans-serif');
+		.attr('transform', `translate(0, ${innerHeight})`)
+		.select('.domain')
+		.remove();
 
 	// Draw chart based on data
 	g.selectAll('rect')
 		.data(data)
-		.enter(data)
-		.append('rect')
+		.join('rect')
 		.attr('y', (d) => yScale(yValue(d)))
 		.attr('width', (d) => xScale(xValue(d)))
 		.attr('height', yScale.bandwidth())
